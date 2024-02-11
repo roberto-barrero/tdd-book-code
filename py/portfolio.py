8e8d3cd5a654b52e37a1be7e94c81286388f9b01
Roberto Barrero <robertobarrerosantoyo@hotmail.com>
2024-02-11 01:50:26 -0600

    feat: convert from EUR to USD

--- py/portfolio.py
+++ py/portfolio.py
@@ -6,11 +6,18 @@ from money import Money
 class Portfolio:
     def __init__(self):
         self.moneys = []
+        self._eur_to_usd = 1.2
 
     def add(self, *moneys):
         self.moneys.extend(moneys)
 
     def evaluate(self, currency):
         total = functools.reduce(
-            operator.add, map(lambda money: money.amount, self.moneys), 0)
+            operator.add, map(lambda money: self.__convert(money, currency), self.moneys), 0)
         return Money(total, currency)
+
+    def __convert(self, money, currency):
+        if money.currency == currency:
+            return money.amount
+        else:
+            return money.amount * self._eur_to_usd
